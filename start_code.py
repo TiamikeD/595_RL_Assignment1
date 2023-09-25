556fe675657e45c2f8dfcf0a06de40a45f9d468b
tiadud@unm.edu <tiamike.dduely@gmail.com>
2023-09-25 12:13:23 -0600

    Merged plot library and added jacobian, gdop, reward

--- start_code.py
+++ start_code.py
@@ -3,38 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 import sympy
+import bandit as bd
 
 np.random.seed(19680801)
 
 POSSIBLE_ACTIONS = ["012","013","014","023","024","034","123","134","234","124"]
-
-def plot3d(anchor_points = [], target_point = [], iterator = []): # KEITH
-    print(anchor_points)
-
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-
-    for point in anchor_points:
-        ax.scatter(point[0],point[1], point[2], c="blue", marker="D")
-
-    ax.scatter(target_point[0], target_point[1], target_point[2], c="red", marker="X")
-
-    ax.scatter(iterator[0], iterator[1], iterator[2], c="green")
-
-    blue_diamond = mlines.Line2D([], [], color='blue', marker='D', linestyle='None', markersize=10, label='Anchor Point')
-    red_x = mlines.Line2D([], [], color='red', marker='X', linestyle='None', markersize=10, label='Target')
-    green_dot = mlines.Line2D([], [], color='green', marker='.',linestyle='None', markersize=10, label='Iterator')
-
-    plt.legend(handles=[blue_diamond, red_x, green_dot])
-
-
-    ax.set_xlabel('X Axis')
-    ax.set_ylabel('Y Axis')
-    ax.set_zlabel('Z Axis')
-
-    plt.show()
-
-    return
 
 def choose_anchors(anchors, epsilon): # TIAMIKE
     # explore = np.random.binomial(1, epsilon) # Decide to explore or not
@@ -46,11 +19,12 @@ def choose_anchors(anchors, epsilon): # TIAMIKE
         # recs = [1,2,3]
     return recs
 
-def calculate_q_values(rewards = [], action_count=0):
+def calculate_q_value(reward, prev_q, action_count=0):
     # Qn = (R1+R2+...+Rn) / (n-1)
     # When recalculating, use the existing Q values instead of recalculating:
     # Q(n+1) = (1/n)(Rn - Qn)   (pg. 31 of textbook)
-    return [0,0,0,0,0]
+    # Q = (1 / action_count) * reward * 
+    return [0]
 
 ### Step 1: Initialize the problem parameters.
 
@@ -97,6 +71,7 @@ def calculate_reward(gdop):
 for epsilon in epsilons:
 
     # Initializing the 'position_stimate' to 'position_initial_estimate'
+    # p(hat) ^ (ite)=0
     position_estimate = position_initial_estimate.copy()
 
 
@@ -104,9 +79,11 @@ for epsilon in epsilons:
     action_count = 0
 
     # Initialize Q-values for each epsilon
-    # Tiamike: Also initializing R-values. 5 elements for 5 anchors.
-    Q_of_a = [0,0,0,0,0]
-    R_of_a = [0,0,0,0,0]
+    # Tiamike: Also initializing R-values. These are single digit numbers 
+    # because each action has only one reward associated
+    prev_q = 0
+    Q_of_a = 0
+    R_of_a = 0
 
     # Main loop for the epsilon-greedy bandit algorithm
     for i in range(total_steps):
@@ -119,8 +96,6 @@ for epsilon in epsilons:
         chosen_anchors = choose_anchors(anchor_labels, epsilon)
         print( "Chosen Anchors: " + str(chosen_anchors) )
 
-# plot3d(anchor_positions, target_position, position_initial_estimate)
-
         # Tiamike: I'm assuming selected_positions are the positions of the anchors that were chosen.
         selected_positions = []
         for index in chosen_anchors:
@@ -130,23 +105,56 @@ for epsilon in epsilons:
         # selected_positions = [i for i in range(10)]
 
         # Code for determining pseudoranges
+        # These pseudoranges are the 3 distances from the 3 anchors
+        # i.e., the f, g, and h functions
+        # They are the MEASURED distances so they have a small amount of noise.
         pseudoranges = [euclidean_distance(selected_positions[i], position_estimate) + np.random.uniform(-0.0001, 0.0001, 1)[0] for i in range(num_anchors_to_choose)]
-        print("Pseudoranges: " + str(pseudoranges))
+        print( "Pseudoranges: " + str(pseudoranges) )
+        print( "Initial position estimate: " + str(position_estimate) )
 
         # Determine the 'jacobian' matrix based on the selected anchor nodes
 
+        # A = ( Xa - X^t )^2 + ( Ya - Y^t )^2 + (Za - Z^t)^2
+        # FA, GA, HA are the constants for function f, g and h
+        FA = (selected_positions[0][0] - position_estimate[0])**2 + (selected_positions[0][1] - position_estimate[1])**2 + (selected_positions[0][2] - position_estimate[2])**2
+        # print("FA: " + str(FA))
+        GA = (selected_positions[1][0] - position_estimate[0])**2 + (selected_positions[1][1] - position_estimate[1])**2 + (selected_positions[1][2] - position_estimate[2])**2
+        HA = (selected_positions[2][0] - position_estimate[0])**2 + (selected_positions[2][1] - position_estimate[1])**2 + (selected_positions[2][2] - position_estimate[2])**2
+
+        # fa, fb, fc are the first row of the jacobian matrix
+        fa = - 1 / (FA**(0.5)) * (selected_positions[0][0] - position_estimate[0])
+        # print("fa: " + str(fa))
+        fb = - 1 / (FA**(0.5)) * (selected_positions[1][0] - position_estimate[1])
+        fc = - 1 / (FA**(0.5)) * (selected_positions[2][0] - position_estimate[2])
+
+        # ga, gb, gc are the second row of the jacobian matrix
+        ga = - 1 / (GA**(0.5)) * (selected_positions[0][1] - position_estimate[0])
+        gb = - 1 / (GA**(0.5)) * (selected_positions[1][1] - position_estimate[1])
+        gc = - 1 / (GA**(0.5)) * (selected_positions[2][1] - position_estimate[1])
+
+        # ga, gb, gc are the third row of the jacobian matrix
+        ha = - 1 / (HA**(0.5)) * (selected_positions[0][2] - position_estimate[0])
+        hb = - 1 / (HA**(0.5)) * (selected_positions[1][2] - position_estimate[0])
+        hc = - 1 / (HA**(0.5)) * (selected_positions[2][2] - position_estimate[0])
+
+        jacobian_matrix = np.array([[fa, fb, fc],
+                                    [ga, gb, gc],
+                                    [ha, hb, hc]])
+        print("Jacobian matrix: " + str(jacobian_matrix))
 
         # Determine the 'gdop' value GDOP(A) from the calculated 'jacobian'
-
-
+        gdop = calculate_gdop(jacobian_matrix)
+        print( "GDOP: " + str(gdop) )
         # Determine the 'reward' R(A) using the 'gdop' value
-        # R_of_a = 
+        R_of_a = calculate_reward(gdop)
+        print( "Reward: " + str(R_of_a) )
 
         # Update action counts N(A)
-        # action_count++
+        action_count += 1
 
         # Update Q-values Q(A)
-        # Q_of_a = 
+        Q_of_a = calculate_q_value(R_of_a, action_count)
+        prev_q = Q_of_a
 
         # Update position estimate
         # position_estimate = 
@@ -168,3 +176,8 @@ for epsilon in epsilons:
 
 
 # Plot Distance Error vs. Steps for each step and each epsilon
+iterator_points = np.array([0,0,0])
+for i in range(10):
+    iterator_points = np.vstack([iterator_points, np.array([i,i,i])])
+
+bd.plot3d(anchor_positions, target_position, position_initial_estimate, iterator_points)

abbc261b9d6ce4793115babc4213ef34e17e5246
keith <keithhbova@gmail.com>
2023-09-28 12:01:14 -0600

    cleaning up code a bit

--- bandit.py
+++ bandit.py
@@ -43,3 +43,69 @@ def plot3d(anchor_points = [], target_point = [], position_initial_estimate = []
     plt.show()
 
     return
+
+
+def get_jacobian_matrix(selected_positions, position_estimate):
+        FA = (selected_positions[0][0] - position_estimate[0])**2 + (selected_positions[0][1] - position_estimate[1])**2 + (selected_positions[0][2] - position_estimate[2])**2
+        GA = (selected_positions[1][0] - position_estimate[0])**2 + (selected_positions[1][1] - position_estimate[1])**2 + (selected_positions[1][2] - position_estimate[2])**2
+        HA = (selected_positions[2][0] - position_estimate[0])**2 + (selected_positions[2][1] - position_estimate[1])**2 + (selected_positions[2][2] - position_estimate[2])**2
+
+        fa = - 1 / (FA**(0.5)) * (selected_positions[0][0] - position_estimate[0])
+        fb = - 1 / (FA**(0.5)) * (selected_positions[1][0] - position_estimate[1])
+        fc = - 1 / (FA**(0.5)) * (selected_positions[2][0] - position_estimate[2])
+
+        ga = - 1 / (GA**(0.5)) * (selected_positions[0][1] - position_estimate[0])
+        gb = - 1 / (GA**(0.5)) * (selected_positions[1][1] - position_estimate[1])
+        gc = - 1 / (GA**(0.5)) * (selected_positions[2][1] - position_estimate[1])
+
+        ha = - 1 / (HA**(0.5)) * (selected_positions[0][2] - position_estimate[0])
+        hb = - 1 / (HA**(0.5)) * (selected_positions[1][2] - position_estimate[0])
+        hc = - 1 / (HA**(0.5)) * (selected_positions[2][2] - position_estimate[0])
+
+        return np.array([[fa, fb, fc],[ga, gb, gc],[ha, hb, hc]])
+
+def get_action_index(recs, POSSIBLE_ACTIONS): # KEITH
+    for action_index, action in enumerate(POSSIBLE_ACTIONS):
+        if((recs == action).all()):
+            return action_index
+    return -1
+
+def get_new_deltas_to_calculate_new_position(jacobian, pseudoranges):
+    jacobian_transpose = jacobian.T
+    jacobian_dot_jacobian_transpose = np.dot(jacobian.T, jacobian)
+    inverse_matrix = np.linalg.inv(jacobian_dot_jacobian_transpose)
+    multiply_me_with_residual = np.dot(inverse_matrix, jacobian_transpose)
+    residual = np.array( [ [pseudoranges[0]], [pseudoranges[1]], [pseudoranges[2]] ] )
+    return_me = np.dot(multiply_me_with_residual, residual)
+    return return_me
+
+
+def choose_anchors(anchors, anchor_positions, epsilon, num_anchors_to_choose): # TIAMIKE
+    # explore = np.random.binomial(1, epsilon) # Decide to explore or not
+    # if explore == 1: # Choose 3 random anchors (Explore)
+    recs = np.random.choice(anchors, size=num_anchors_to_choose, replace=False)
+    # else: # Choose most promising anchors (Exploit)
+        # Rank anchors by their reward
+        # Choose the three highest ranked anchors
+    recs = np.sort(recs)
+    return recs
+
+def calculate_q_value(reward, previous_q_value, action_count):
+    # Qn = (R1+R2+...+Rn) / (n-1)
+    # When recalculating, use the existing Q values instead of recalculating:
+    # Q(n+1) = (1/n)(Rn - Qn)   (pg. 31 of textbook)
+    Q = (1 / action_count) * (reward - previous_q_value)
+    return Q
+
+
+def euclidean_distance(a, b):
+    return np.linalg.norm(a - b)
+
+def calculate_gdop(jacobian):
+    G = np.linalg.inv(np.dot(jacobian.T, jacobian))
+    gdop = np.sqrt(np.trace(G))
+    return gdop
+
+
+def calculate_reward(gdop):
+    return np.sqrt(10/3) / gdop if gdop > 0 else 0
